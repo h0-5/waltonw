2ea11ceec4a465cc787736eacae049311e10307d
Walton Family <waltonfamily@gmail.com>
2026-09-07 00:01:28 +0300

    fix: remove MySQL session store, use memory sessions

--- config/session.js
+++ config/session.js
@@ -1,25 +1,7 @@
 const session = require('express-session');
-const MySQLStore = require('express-mysql-session')(session);
-const db = require('./database');
-
-const sessionStore = new MySQLStore({
-  clearExpired: true,
-  checkExpirationInterval: 900000,
-  expiration: parseInt(process.env.SESSION_MAX_AGE) || 2592000000,
-  createDatabaseTable: true,
-  schema: {
-    tableName: 'wf_sessions',
-    columnNames: {
-      session_id: 'session_id',
-      expires: 'expires',
-      data: 'data'
-    }
-  }
-}, db);
 
 const sessionConfig = {
   secret: process.env.SESSION_SECRET || 'walton_family_secret',
-  store: sessionStore,
   resave: false,
   saveUninitialized: false,
   cookie: {
